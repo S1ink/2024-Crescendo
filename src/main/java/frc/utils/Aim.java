dbb562e22aae3bdafac0bfe74f84eac59e642a03
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2024-01-16 19:42:45 -0600

    notes

--- src/main/java/frc/utils/Aim.java
+++ src/main/java/frc/utils/Aim.java
@@ -2,4 +2,23 @@ package frc.utils;
 
 public class Aim {
 	// insert particle physics
+
+	/**
+	 * Takes the current position from VISION and aims the output device towards speaker
+	 */
+
+	// move towards target
+	public void moveTowardsTarget() {
+		// do stuff
+	}
+
+	// aim (update theta)
+	public void rotationAim() {
+		// naming is hard; guh
+	}
 }
+
+// user presses shoot trigger
+// locks controls for 3s | command finished | cancel button
+// do the thing
+// unlock controls
